5b1b35c2a6d997f00774d7e14a2d22b175fea053
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-01-13 00:35:10 +0530

    Time: 16 ms (58.92%) | Memory: 45.8 MB (50.21%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -1,42 +1,32 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        Stack<Integer>open=new Stack<>();
-        Stack<Integer>openclose=new Stack<>();
-        int n = s.length();
-        if(n%2==1) return false;
+        if(s.length()%2!=0){
+            return false;
+        }
+        //left and right count method.....
+        int leftcount=0;
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(locked.charAt(i)=='0'){
-                openclose.push(i);
-            }
-            else if(ch==')'){
-                if(!open.isEmpty()){
-                    open.pop();
-                }else{
-                    if(!openclose.isEmpty()){
-                        if(openclose.peek()<i){
-                            openclose.pop();
-                        }
-                    }else{
-                        return false;
-                    }
-                }
+            if(s.charAt(i)=='(' || locked.charAt(i)=='0'){
+                leftcount++;
             }
             else{
-                open.push(i);
+                leftcount--;
             }
-        }
-        while(!open.isEmpty()&&!openclose.isEmpty()){
-            if(open.peek()>openclose.peek()){
+            if(leftcount<0){
                 return false;
             }
-            else{
-                open.pop();
-                openclose.pop();
-            }
         }
-        if(!open.isEmpty()){
-            return false;
+        int rightcount=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==')' || locked.charAt(i)=='0'){
+                rightcount++;
+            }
+            else{
+                rightcount--;
+            }
+            if(rightcount<0){
+                return false;
+            }
         }
         return true;
     }
