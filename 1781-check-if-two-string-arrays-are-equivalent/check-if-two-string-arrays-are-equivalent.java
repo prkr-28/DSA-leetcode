94d40e24fc3af361b943246d602c6522b8e43146
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-01-14 11:20:58 +0530

    Time: 1 ms (60.07%) | Memory: 41.1 MB (95.01%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -8,17 +8,10 @@ class Solution {
         for(int i=1;i<word2.length;i++){
             s2+=word2[i];
         }
-        char[] s1arr=s1.toCharArray();
-        char[] s2arr=s2.toCharArray();
-        int maxsize=Math.max(s1arr.length,s2arr.length);
-        for(int i=0;i<maxsize;i++){
-            if(i<s1arr.length&&i<s2arr.length&&s1arr[i]==s2arr[i]){
-                continue;
-            }
-            else{
-                return false;
-            }
+        if(s1.equals(s2)){
+            return true;
+        }else{
+            return false;
         }
-        return true;
     }
 }
