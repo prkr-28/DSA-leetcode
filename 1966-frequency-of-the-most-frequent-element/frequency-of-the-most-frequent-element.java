d185e9d1152868df56cfa19b5b779de656d8d0f7
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-01-15 18:06:01 +0530

    Time: 28 ms (98.55%) | Memory: 56.6 MB (60.76%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,44 +1,19 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        int result=Integer.MIN_VALUE;
-        int[] prefixsum=new int[nums.length];
-        prefixsum[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-        }
-        for(int i=0;i<nums.length;i++){
-            int freq=bsearch(i,nums,k,prefixsum);
-            result=Math.max(result,freq);
-        }
-        return result;
-    }
-    public static int bsearch(int i,int[] nums,int k,int[] prefixsum){
-        int l=0;
-        int r=i;
-        int res=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            int count=(i-mid+1);
-            int winsum=count*nums[i];
-            int originalsum=prefixsum[i]-prefixsum[mid]+nums[mid];
-            int oper=winsum-originalsum;
-            if(oper>k){
-                l=mid+1;
-            }
-            else{
-                res=mid;
-                r=mid-1;
-            }
-        }
-        return i-res+1;
-    }
+        int left = 0;
+        long currSum = 0; 
+        int maxFreq = 0;
+        int n = nums.length;
 
-    // public static int sum(int[] nums,int si,int ei){
-    //     int sum=0;
-    //     for(int i=si;i<=ei;i++){
-    //         sum+=nums[i];
-    //     }
-    //     return sum;
-    // }
+        for (int right = 0; right < n; right++) {
+            currSum += nums[right];
+            while ((long) nums[right] * (right - left + 1) - currSum > k) {
+                currSum -= nums[left]; // Shrink the window from the left
+                left++;
+            }
+            maxFreq = Math.max(maxFreq, right - left + 1);
+        }
+        return maxFreq;
+    }
 }
