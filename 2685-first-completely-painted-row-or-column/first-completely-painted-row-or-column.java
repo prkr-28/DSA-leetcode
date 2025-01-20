4d97910f3d913266635e02f0d591da012a7cddf7
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-01-20 09:39:24 +0530

    Time: 30 ms (42.29%) | Memory: 68.8 MB (44.28%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -2,24 +2,31 @@ class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        Map<Integer,Integer>rowpos=new HashMap<>();
-        Map<Integer,Integer>colpos=new HashMap<>();
-        int[] rowcount=new int[n];
-        int[] colcount=new int[m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                rowpos.put(mat[i][j],i);
-                colpos.put(mat[i][j],j);
-            }
-        }
+
+        Map<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
-            int v=arr[i];
-            rowcount[rowpos.get(v)]++;
-            colcount[colpos.get(v)]++;
-            if(rowcount[rowpos.get(v)]==m||colcount[colpos.get(v)]==n){
-                return i;
-            }
+            map.put(arr[i],i);
         }
-        return -1;
+
+        int bestidx=Integer.MAX_VALUE;
+        for(int row=0;row<n;row++){
+            int lastidx=0;
+            for(int col=0;col<m;col++){
+                int val=mat[row][col];
+                int idx=map.get(val);
+                lastidx=Math.max(lastidx,idx);
+            }
+            bestidx=Math.min(bestidx,lastidx);
+        }
+        for(int col=0;col<m;col++){
+            int lastidx=0;
+            for(int row=0;row<n;row++){
+                int val=mat[row][col];
+                int idx=map.get(val);
+                lastidx=Math.max(lastidx,idx);
+            }
+            bestidx=Math.min(bestidx,lastidx);
+        }
+        return bestidx;
     }
 }
