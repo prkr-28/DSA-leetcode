301c4fbdc671d5a4bf58707d8fd174bf38330e84
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-01-27 11:13:53 +0530

    Time: 19 ms (56.10%) | Memory: 44.1 MB (90.04%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,81 +1,14 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        return sumSubarrayMax(nums)-sumSubarrayMins(nums);
-    }
-    public long sumSubarrayMins(int[] arr) {
-        long maxscore = 0;
-        //int mod=1000000007;
-        int n=arr.length;
-        int[] nsl=getnsl(arr,n);
-        int[] nsr=getnsr(arr,n);
-        for (int i = 0; i < n; i++) {
-            long d1=i-nsl[i];
-            long d2=nsr[i]-i;
-            maxscore=(maxscore+(d1*d2*arr[i]));
-        }
-        return maxscore;
-    }
-    public long sumSubarrayMax(int[] arr) {
-        long maxscore = 0;
-        //int mod=1000000007;
-        int n=arr.length;
-        int[] nsl=getngl(arr,n);
-        int[] nsr=getngr(arr,n);
-        for (int i = 0; i < n; i++) {
-            long d1=i-nsl[i];
-            long d2=nsr[i]-i;
-            maxscore=(maxscore+(d1*d2*arr[i]));
-        }
-        return maxscore;
-    }
-    public static int[] getnsl(int[] arr,int n){
-        int[] result=new int[n];
-        Stack<Integer>st=new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&arr[st.peek()]>arr[i]){
-                st.pop();
+    public long subArrayRanges(int[] A) {
+        long res = 0;
+        for (int i = 0; i < A.length; i++) {
+            int max = A[i], min = A[i];
+            for (int j = i; j < A.length; j++) {
+                max = Math.max(max, A[j]);
+                min = Math.min(min, A[j]);
+                res += max - min;
             }
-            result[i]=st.isEmpty()?-1:st.peek();
-            st.push(i);
         }
-        return result;
-    }
-    public int[] getnsr(int[] arr, int n) {
-        int[] result = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
-            }
-            result[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-
-        return result;
-    }
-    public static int[] getngl(int[] arr,int n){
-        int[] result=new int[n];
-        Stack<Integer>st=new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&arr[st.peek()]<arr[i]){
-                st.pop();
-            }
-            result[i]=st.isEmpty()?-1:st.peek();
-            st.push(i);
-        }
-        return result;
-    }
-    public int[] getngr(int[] arr, int n) {
-        int[] result = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
-                st.pop();
-            }
-            result[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-
-        return result;
+        return res;
     }
 }
