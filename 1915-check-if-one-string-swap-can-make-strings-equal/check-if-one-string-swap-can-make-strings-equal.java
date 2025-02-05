1ed8b5fe7a3962188974887d6ec4aaec1737df95
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-02-05 08:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (89.79%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,29 +1,25 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        Map<Character,Integer>map=new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            map.put(s1.charAt(i),map.getOrDefault(s1.charAt(i),0)+1);
-        }
-        for(int i=0;i<s2.length();i++){
-            map.put(s2.charAt(i),map.getOrDefault(s2.charAt(i),0)-1);
-        }
-
-        for(char ch:map.keySet()){
-            if(map.get(ch)>=1){
-                return false;
-            }
-        }
         int count=0;
+        int i_=-1;
+        int j_=-1;
         for(int i=0;i<s1.length();i++){
             if(s1.charAt(i)!=s2.charAt(i)){
                 count++;
+                if(i_==-1){
+                    i_=i;
+                }
+                else if(j_==-1){
+                    j_=i;
+                }
             }
         }
-        if(count==2||count==0){
+        if(count==0){
             return true;
         }
-        else{
-            return false;
+        else if(count==2&&s1.charAt(i_)==s2.charAt(j_)&&s1.charAt(j_)==s2.charAt(i_)){
+            return true;
         }
+        return false;
     }
 }
