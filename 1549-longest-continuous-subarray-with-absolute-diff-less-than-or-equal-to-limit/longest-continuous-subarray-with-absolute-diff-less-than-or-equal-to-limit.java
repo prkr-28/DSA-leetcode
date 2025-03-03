5693cb2425122d7c2db51db35f2f1752b0a47095
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-03 10:53:51 +0530

    Time: 135 ms (13.97%) | Memory: 59.5 MB (28.33%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,29 +1,17 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int f,int s){
-        this.first=f;
-        this.second=s;
-    }
-}
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        PriorityQueue<Pair>minheap=new PriorityQueue<>((a, b) -> Integer.compare(a.first, b.first));
-        PriorityQueue<Pair>maxheap=new PriorityQueue<>((a, b) -> Integer.compare(b.first, a.first));
+        TreeMap<Integer,Integer>map=new TreeMap<>();
         int i=0;
         int j=0;
         int maxlength=0;
         while(j<nums.length){
-            minheap.add(new Pair(nums[j],j));
-            maxheap.add(new Pair(nums[j],j));
-            while(maxheap.peek().first-minheap.peek().first>limit){
-                i=Math.min(maxheap.peek().second,minheap.peek().second)+1;
-                while(maxheap.peek().second<i){
-                    maxheap.poll();
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            while(map.lastKey()-map.firstKey()>limit){
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0){
+                    map.remove(nums[i]);
                 }
-                while(minheap.peek().second<i){
-                    minheap.poll();
-                }
+                i++;
             }
             maxlength=Math.max(maxlength,j-i+1);
             j++;
