7b7a0868e7219d1033b392d060a62ee385290185
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-04 08:25:42 +0530

    Time: 1 ms (27.27%) | Memory: 41.1 MB (34.26%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -3,10 +3,8 @@ class Solution {
         if(n==0){
             return false;
         }
-        while(n%4==0){
-            n=n/4;
-        }
-        if(n==1){
+        int x=(int)(Math.log(n)/Math.log(4));
+        if(Math.pow(4,x)==n){
             return true;
         }
         return false;
