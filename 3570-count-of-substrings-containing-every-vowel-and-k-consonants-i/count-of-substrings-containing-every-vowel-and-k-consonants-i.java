cf70ba0872b3fa316e092dad20ff9507088f06df
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-10 21:52:34 +0530

    Time: 1631 ms (5.23%) | Memory: 45.8 MB (7.84%) - LeetSync

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.java
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -1,65 +1,35 @@
 class Solution {
-    public boolean isVowel(char ch){
+    public int countOfSubstrings(String word, int k) {
+        int n=word.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(word.substring(i,j+1),k)==true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public boolean isvowel(char ch){
         return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
-    public int countOfSubstrings(String word, int k) {
-        int n = word.length();
-        Map<Character, Integer> mp = new HashMap<>(); // to keep count of vowels in current window
-        
-        // Preprocessing to find index of next consonant
-        int[] nextCons = new int[n];
-        int lastConsIdx = n;
-        for (int i = n - 1; i >= 0; i--) {
-            nextCons[i] = lastConsIdx;
-            if (!isVowel(word.charAt(i))) {
-                lastConsIdx = i;
+    public boolean check(String s,int k){
+        //int vcount=0;
+        int conso=0;
+        Set<Character>set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(isvowel(ch)){
+                set.add(ch);
+            }
+            else{
+                conso++;
             }
         }
-        
-        int i = 0, j = 0;
-        int count = 0;
-        int cons = 0;
-        
-        while (j < n) {
-            char ch = word.charAt(j);
-            if (isVowel(ch)) {
-                mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-            } else {
-                cons++;
-            }
-            
-            while (cons > k) {
-                char ci = word.charAt(i);
-                if (isVowel(ci)) {
-                    mp.put(ci, mp.get(ci) - 1);
-                    if (mp.get(ci) == 0) {
-                        mp.remove(ci);
-                    }
-                } else {
-                    cons--;
-                }
-                i++;
-            }
-            
-            while (i < n && mp.size() == 5 && cons == k) {
-                int idx = nextCons[j];
-                count += idx - j;
-                
-                char ci = word.charAt(i);
-                if (isVowel(ci)) {
-                    mp.put(ci, mp.get(ci) - 1);
-                    if (mp.get(ci) == 0) {
-                        mp.remove(ci);
-                    }
-                } else {
-                    cons--;
-                }
-                i++;
-            }
-            
-            j++;
+        if(set.size()==5&&conso==k){
+            return true;
         }
-        
-        return count;
+        return false;
     }
 }
