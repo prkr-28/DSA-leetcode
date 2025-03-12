f91c04dec77ca00d336f94f2a820025d7ea0d1c3
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-12 09:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (48.41%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,10 +1,21 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        IntPredicate lambdap=num->num>0;
-         IntPredicate lambdan=num->num<0;
-        int pos=(int)Arrays.stream(nums).filter(lambdap).count();
-        int neg=(int)Arrays.stream(nums).filter(lambdan).count();
-
-         return Math.max(pos,neg);
+        int pos=nums.length-lowerbound(nums,1);
+        int neg=lowerbound(nums,0);
+        return Math.max(pos,neg);
+    }
+    public int lowerbound(int[] nums,int target){
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return l;
     }
 }
