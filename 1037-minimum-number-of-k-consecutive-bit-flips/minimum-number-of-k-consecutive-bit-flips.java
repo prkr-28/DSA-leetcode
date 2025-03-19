c9962d92faddad9f4e6023f0409eac2fcf238634
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-19 09:25:47 +0530

    Time: 5 ms (74.79%) | Memory: 54.9 MB (95.73%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -3,11 +3,13 @@ class Solution {
         int ans=0;
         int flips=0;
         int n=nums.length;
-        int[] flipped=new int[n];
-        Arrays.fill(flipped,0);
+        // int[] flipped=new int[n];
+        // Arrays.fill(flipped,0);
         for(int i=0;i<n;i++){
             if(i>=k){
-                flips-=flipped[i-k];
+                if(nums[i-k]==2){
+                    flips-=1;
+                }
             }
             if((nums[i]==1&&flips%2==1)||nums[i]==0&&flips%2==0){
                 if(i+k>n){
@@ -15,7 +17,7 @@ class Solution {
                 }
                 ans++;
                 flips++;
-                flipped[i]=1;
+                nums[i]=2;
             }
         }
         return ans;
