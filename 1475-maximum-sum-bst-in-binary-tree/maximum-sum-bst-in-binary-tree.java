1250929fb0de4c8494a79886813f356894a91536
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-03-20 21:55:21 +0530

    Time: 7 ms (51.84%) | Memory: 60.1 MB (29.33%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Nodevalues{
+    int maxsum;
+    int maxnode;
+    int minnode;
+    boolean isbst;
+    public Nodevalues(int a,int b,int c,boolean d){
+        this.maxsum=a;
+        this.maxnode=b;
+        this.minnode=c;
+        this.isbst=d;
+    }
+}
+class Solution {
+    int maxsum=0;
+    public int maxSumBST(TreeNode root) {
+        solve(root);
+        return maxsum;
+    }
+    public Nodevalues solve(TreeNode root){
+        if(root==null){
+            return new Nodevalues(0,Integer.MIN_VALUE,Integer.MAX_VALUE,true);
+        }
+        Nodevalues leftv=solve(root.left);
+        Nodevalues rightv=solve(root.right);
+        if(leftv.isbst==true&&rightv.isbst==true&&leftv.maxnode<root.val&&root.val<rightv.minnode){
+            maxsum=Math.max(maxsum,leftv.maxsum+rightv.maxsum+root.val);
+            return new Nodevalues(leftv.maxsum+rightv.maxsum+root.val,Math.max(root.val,rightv.maxnode),Math.min(root.val,leftv.minnode),true);
+        }
+
+        return new Nodevalues(Math.max(leftv.maxsum,rightv.maxsum),Integer.MAX_VALUE,Integer.MIN_VALUE,false);
+    }
+}
