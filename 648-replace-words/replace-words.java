1d283170461b26bee352e9a2e19c0a7459528190
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-04-02 11:15:02 +0530

    Time: 697 ms (20.44%) | Memory: 55.4 MB (63.71%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set<String>set=new HashSet<>(dictionary);
+        String[] words=sentence.split(" ");
+
+        StringBuilder res=new StringBuilder();
+
+        for(String s:words){
+            res.append(findword(s,set)).append(" ");
+        }
+        res.deleteCharAt(res.length()-1);
+        return res.toString();
+    }
+
+    public String findword(String s,Set<String>set){
+        for(int i=1;i<=s.length();i++){
+            String temp=s.substring(0,i);
+            if(set.contains(temp)){
+                return temp;
+            }
+        }
+        return s;
+    }
+}
