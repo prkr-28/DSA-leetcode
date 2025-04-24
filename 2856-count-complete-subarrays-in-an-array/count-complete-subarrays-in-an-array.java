a3ca507eac07611db81ea053c1d697cc27c79532
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-04-24 14:04:36 +0530

    Time: 7 ms (89.07%) | Memory: 45.4 MB (23.52%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,20 +1,26 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int n=nums.length;
-        int count=0;
         Set<Integer>set=new HashSet<>();
         for(int i:nums){
             set.add(i);
         }
-
-        for(int i=0;i<n;i++){
-            Set<Integer>tempset=new HashSet<>();
-            for(int j=i;j<n;j++){
-                tempset.add(nums[j]);
-                if(tempset.size()==set.size()){
-                    count++;
+        int k=set.size();
+        int count=0;
+        int i=0;
+        int j=0;
+        Map<Integer,Integer>map=new HashMap<>();
+        while(j<n){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            while(map.size()==k&&i<n){
+                count+=n-j;
+                map.put(nums[i],map.get(nums[i])-1);
+                if (map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
                 }
+                i++;
             }
+            j++;
         }
         return count;
     }
