bafe513f4d5aa882d017b6439b7d0fabd4065126
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2025-05-06 09:04:38 +0530

    Time: 2 ms (7.30%) | Memory: 45.5 MB (27.48%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,9 +1,12 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] res=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            res[i]=nums[nums[i]];
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]+n*(nums[nums[i]]%n);
         }
-        return res;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]/n;
+        }
+        return nums;
     }
 }
